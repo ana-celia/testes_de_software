f00cb78d827d43bd49bb54197ebcc2b7e248208b
Ana Célia Araújo <112821127+ana-celia@users.noreply.github.com>
2024-03-31 21:35:47 -0300

    Update tests.py
    correçoes para cada condição ser um caso de teste

--- triangulo/tests.py
+++ triangulo/tests.py
@@ -73,25 +73,27 @@ class TesteTrianguloEquilatero (unittest.TestCase):
         self.assertFalse(t.ehEquilatero())
 
 class TesteTrianguloIsosceles (unittest.TestCase):
-    def test_ehIsosceles (self): #válidos valores de a, b, c - sendo um lado diferente e dois lados iguais
+    def test_ehIsoscelesAB (self): #válidos valores de a, b, c - sendo um lado diferente e dois lados iguais
         a, b, c = 5, 5, 6
         t = Triangulo(a, b, c)
         self.assertTrue(t.ehIsosceles())
 
+    def test_ehIsoscelesAC (self):
         a, b, c = 5, 6, 5
         t = Triangulo(a, b, c)
         self.assertTrue(t.ehIsosceles())
 
+    def test_ehIsoscelesBC (self):
         a, b, c = 6, 5, 5
         t = Triangulo(a, b, c)
         self.assertTrue(t.ehIsosceles())
 
-    def test_iguais (self): #inválidos valores de a, b, c
+    def test_ehIsoscelesiguais (self): #inválidos valores de a, b, c
         a, b, c = 5, 5, 5
         t = Triangulo(a, b, c)
         self.assertFalse(t.ehIsosceles())
 
-    def test_diferentes (self): #inválidos valores de a, b, c
+    def test_ehIsoscelesdiferentes (self): #inválidos valores de a, b, c
         a, b, c = 5, 6, 7
         t = Triangulo(a, b, c)
         self.assertFalse(t.ehIsosceles())
@@ -103,15 +105,17 @@ class TesteTrianguloEscaleno (unittest.TestCase): #válidos valores de a, b, c -
         t = Triangulo(a, b, c)
         self.assertTrue(t.ehEscaleno())
 
-    def test_dois_iguais (self):
+    def test_ehEscaleno_AB (self):
         a, b, c = 5, 5, 6   #inválidos valores de a e b
         t = Triangulo(a, b, c)
         self.assertFalse(t.ehEscaleno())
 
+   def test_ehEscaleno_AC (self):
         a, b, c = 5, 6, 5   #inválidos valores de a e c
         t = Triangulo(a, b, c)
         self.assertFalse(t.ehEscaleno())
-
+   
+   def test_ehEscaleno_BC (self):
         a, b, c = 5, 6, 6   #inválidos valores de b e c
         t = Triangulo(a, b, c)
         self.assertFalse(t.ehEscaleno())
