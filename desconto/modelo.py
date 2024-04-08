ed13eb000e614ac35f09620b695d37149bbf9b75
Ana Célia Araújo <112821127+ana-celia@users.noreply.github.com>
2024-04-07 22:17:34 -0300

    Update modelo.py

--- desconto/modelo.py
+++ desconto/modelo.py
@@ -1,20 +1,20 @@
-import datetime
+'''
+Questão: Contrua uma função (em python) que realize o cálculo do desconto por dependente. A função
+recebe um valor, e a idade do dependente. A idade do dependente deve estar restrita ao inter-
+valo [0..24]. O valor mínimo da compra deve ser 250,00 reais. Para dependentes até 12 anos
+(inclusive) o desconto é de 15%. Entre 13 e 18 (inclusive) o desconto é de 12%. Dos 19 aos 21
+(inclusive) o desconto é de 5% e dos 22 aos 24 de 3%.'''
 
-class Desconto_idade_dependente:
-    def __init__(self, valor_compra, idade_dependente):
-        self.valor_compra = valor_compra
-        self.idade_dependente = idade_dependente
-        
-    def calcular_desconto_dependente(valor_compra, idade_dependente):
-        if valor_compra < 250:
-            raise ValueError("O valor mínimo da compra deve ser de R$ 250,00")
-        if idade_dependente < 0 or idade_dependente > 24:
-            raise ValueError("A idade do dependente deve estar no intervalo de 0 a 24 anos")
-        if idade_dependente <= 12:
-            return valor_compra * 0.15
-        elif idade_dependente <= 18:
-            return valor_compra * 0.12
-        elif idade_dependente <= 21:
-            return valor_compra * 0.05
-        else:
-            return valor_compra * 0.03
+def calcular_desconto(valor, idade):
+  if valor < 250:
+    return 0
+  elif idade >= 0 and idade <= 12:
+    return valor * 0.15
+  elif idade >= 13 and idade <= 18:
+    return valor * 0.12
+  elif idade >= 19 and idade <= 21:
+    return valor * 0.05
+  elif idade >= 22 and idade <= 24:
+    return valor * 0.03
+  else:
+    return 0
