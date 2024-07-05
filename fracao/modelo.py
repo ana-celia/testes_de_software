fbe28c4a096c9c2943d81f11e36b9372fb0cabd3
Ana Célia Araújo <112821127+ana-celia@users.noreply.github.com>
2024-07-05 08:21:33 -0300

    Update modelo.py
    corrigir

--- fracao/modelo.py
+++ fracao/modelo.py
@@ -1,1 +1,24 @@
 
+import math
+
+class Fracao:
+    def __init__(self, numerador, denominador):
+        if denominador == 0:
+            raise ValueError(  'Denominador deve ser diferente de zero')
+        
+    #     #reduzindo a fracao
+    # def mdc(self, numerador, denominador):    
+    #     mdc = 1
+    #     for i in range(min(numerador,denominador), 1, -1):
+    #         if numerador % i == 0 and denominador % i == 0:
+    #             mdc = 1
+    #             break
+    #     return mdc
+
+    @property
+    def numerador(self):
+        return self._numerador
+
+    @property
+    def denominador(self):
+        return self._denominador
